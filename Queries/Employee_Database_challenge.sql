90906fb93a9a48934da7db64d55f6270b58f3cf0
AlmirOmerdic <omerdic.almir@mayo.edu>
2022-02-11 23:59:46 -0600

    update1

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,25 @@
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO challenge_info
+FROM employees AS e
+INNER JOIN titles AS ti
+ON(e.emp_no = ti.emp_no)
+WHERE(e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY (e.emp_no) DESC;
+
+SELECT * FROM challenge_info;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
