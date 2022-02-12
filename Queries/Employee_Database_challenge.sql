b0537b15a75ea7f37c8027050b1b56ca2bcb891c
AlmirOmerdic <omerdic.almir@mayo.edu>
2022-02-12 14:54:23 -0600

    update222

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -14,12 +14,33 @@ ORDER BY (e.emp_no) DESC;
 SELECT * FROM challenge_info;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no ,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE(rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.title DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+--INTO 
+FROM employees as e
+INNER JOIN dept_emp as de
+ON(e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON(de.emp_no = ti.emp_no)
+WHERE(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND(de.to_date = '9999-01-01');
